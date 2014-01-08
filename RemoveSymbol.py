1948da45dc44c81fa9a62736974e349c4aa178b1
Li Zhenbo <litimetal@gmail.com>
2014-01-08 11:02:14 +0800

    new file:   RemoveSymbol.py Dirty: Need to merge with AddSymbol in future

---
+++ RemoveSymbol.py
@@ -0,0 +1,27 @@
+import os, sys
+
+if __name__ == '__main__':
+
+    file = sys.argv[1]
+    try:
+        symbol = sys.argv[2]
+        print(symbol)
+    except IndexError:
+        print("Default symbol: >")
+        symbol = '>'
+
+    file = os.path.abspath(file)
+    #print(file)
+    fin = open(file, 'r', encoding='utf-8')
+    lines = fin.readlines()
+    #print(lines)
+    fin.close()
+
+    fout = open(file, 'w', encoding='utf-8')
+    for line in lines:
+        line = line.rstrip()
+        line = line.lstrip(symbol)
+        #print(line)
+        print(line, file=fout)
+    fout.close()
+
